8d22e20a4071f313944cf54cafb9277c263e5e23
sdjim <347511109@qq.com>
2019-12-31 11:41:22 +0800

    no message

---
+++ WXMLocationManager.h
@@ -0,0 +1,29 @@
+//
+//  WXMLocationManager.h
+//  WXMComponentization
+//
+//  Created by sdjim on 2019/12/31.
+//  Copyright © 2019 sdjim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WXMLocation : NSObject
+@property (nonatomic, copy) NSString *state;
+@property (nonatomic, copy) NSString *city;
+@property (nonatomic, copy) NSString *cityCode;
+@property (nonatomic, copy) NSString *detailedAddress;
+@property (nonatomic, copy) NSString *subLocality;
+@property (nonatomic, copy) NSString *thoroughfare;
+@property (nonatomic, copy) NSString *subThoroughfare;
+@end
+
+@interface WXMLocationManager : NSObject
++ (instancetype)sharedInstance;
+- (BOOL)location;
+- (void)setCallback:(void (^)(WXMLocation *location))callback;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WXMLocationManager.m
@@ -0,0 +1,98 @@
+//
+//  WXMLocationManager.m
+//  WXMComponentization
+//
+//  Created by sdjim on 2019/12/31.
+//  Copyright © 2019 sdjim. All rights reserved.
+//
+#import <CoreLocation/CoreLocation.h>
+#import "WXMLocationManager.h"
+
+@interface WXMLocationManager () <CLLocationManagerDelegate>
+@property (nonatomic, strong) CLLocationManager *manager;
+@property (nonatomic, strong) NSDictionary *geocodingDictionary;
+@property (nonatomic, copy) void (^resultsCallback)(WXMLocation *location);
+@end
+
+@implementation WXMLocation @end
+@implementation WXMLocationManager
+
++ (instancetype)sharedInstance {
+    static WXMLocationManager *manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[self alloc] init];
+    });
+    return manager;
+}
+
+#pragma mark - 定位代理
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    
+    /**  地址的编码通过经纬度得到具体的地址 */
+    CLLocation *location = manager.location;
+    CLGeocoder *gecoder = [[CLGeocoder alloc] init];
+    [gecoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = [placemarks firstObject];
+        
+        WXMLocation *location = [[WXMLocation alloc] init];
+        location.state = [placemark.addressDictionary objectForKey:@"State"];
+        location.city = placemark.locality ?: placemark.administrativeArea;
+        location.subLocality = [placemark.addressDictionary objectForKey:@"SubLocality"];
+        location.thoroughfare = placemark.thoroughfare ?: @"";
+        location.subThoroughfare = placemark.subThoroughfare ?: @"";
+        
+        if (self.resultsCallback && location.state && location.city && location.subLocality) {
+            self.resultsCallback(location);
+        } else if (self.resultsCallback) {
+            self.resultsCallback(nil);
+        }
+    }];
+    
+    [self.manager stopUpdatingLocation];
+}
+
+/* 定位权限 */
+- (BOOL)location {
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        /** [self.manager requestAlwaysAuthorization]; */
+        /** [self.manager startUpdatingLocation]; */
+        return YES;
+        
+    } else if ([CLLocationManager locationServicesEnabled] &&
+               ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse ||
+                [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways)) {
+        
+        /** [self.manager requestAlwaysAuthorization]; */
+        /** [self.manager startUpdatingLocation]; */
+        return YES;
+        
+    } else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied) {
+        [self showAlertViewController];
+        return NO;
+    }
+    return NO;
+}
+
+- (void)setCallback:(void (^)(WXMLocation *location))callback {
+    self.resultsCallback = callback;
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [self.manager requestWhenInUseAuthorization];
+    }
+    [self.manager startUpdatingLocation];
+}
+
+- (void)showAlertViewController {
+}
+
+- (CLLocationManager *)manager {
+    if (!_manager) {
+        _manager = [[CLLocationManager alloc] init];
+        _manager.delegate = self;
+    }
+    return _manager;
+}
+
+
+@end
